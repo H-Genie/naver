eb1968c2da877d629c9bc7378158531b482ff1db
H-Genie <61546294+H-Genie@users.noreply.github.com>
2020-06-01 21:07:44 +0900

    Add files via upload

--- source/slide.js
+++ source/slide.js
@@ -3,9 +3,15 @@ var index=0;
 var currentBar;
 var progressBar=document.getElementsByClassName('progressbar');
 var currentNum=document.getElementsByClassName('current_num');
-setInterval(clickNext,5000)
+
+var slideInterval=setInterval(clickNext,5000); //인터벌 최초 실행시키는 "변수"
+function clearInterSlide() {slideInterval=clearInterval(slideInterval);} //변수에다가 인터벌 중지 시키는 함수를 재선언함
+function setInterSlide() {slideInterval=setInterval(clickNext,5000);} //변수에다가 인터벌 다시 실행시키는 함수를 재선언함
+
 
 function clickPrev() {
+    clearInterSlide();
+
     if(index==0) { //첫번째 페이지일때 실행
         slide[slide.length-1].style="left:0; transition: left 0.5s ease-in-out;";
         slide[index].style="left:100%; transition: left 0.5s ease-in-out;";
@@ -38,15 +44,20 @@ function clickPrev() {
         currentBar = (index+1)*(100/(slide.length+1));
         progressBar[index].style.width=currentBar+"%";
         currentNum[index].innerHTML=index+1;
-    }             
+    }       
+    setInterSlide();
 }
 
-function clickNext() {                    
+function clickNext() {          
+    clearInterSlide();
+    
     if(index==0) { //첫번째 페이지일때 실행
         slide[index+1].style="left:0px; transition:left 0.5s ease-in-out";
         slide[index].style="left:-100%; transition:left 0.5s ease-in-out";
         slide[slide.length-1].style="left:100%;";
         index+=1; 
+        //slide[index+1].addEventListener('transitionend',slideInterval);
+        //slide[index+1].removeEventListener('transitionend',slideInterval);
 
         currentBar = (index+1)*(100/(slide.length+1));
         progressBar[index].style.width=currentBar+"%";
@@ -58,6 +69,7 @@ function clickNext() {
         slide[index].style="left:-100%; transition:left 0.5s ease-in-out";
         slide[index-1].style="left:100%;";
         index+=1;
+        
 
         currentBar = (index+1)*(100/(slide.length+1));
         progressBar[index].style.width=currentBar+"%";
@@ -69,9 +81,10 @@ function clickNext() {
         slide[slide.length-1].style="left:-100%; transition:left 0.5s ease-in-out";
         slide[index-1].style="left:100%;";
         index=0;
-
+        
         currentBar = (index+1)*(100/(slide.length+1));
         progressBar[0].style.width=currentBar+"%";
         currentNum[0].innerHTML=index+1;
-    }               
+    }       
+    setInterSlide();    
 }
